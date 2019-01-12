1cc2e20e77e3c8d51b9a7b4565abe06a3e8bd2b5
Hodaka Suzuki <altitude3190@gmail.com>
2019-01-12 21:04:12 +0900

    refactor & fix lint error Word.vue

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -1,43 +1,54 @@
 <template>
   <li class="table-view-cell">
-    <div class="media-body">
-      <span>{{ word.word }}</span><span>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;{{ word.ruby }}</span>
-      <p><span>{{ word.meaning }}</span></p>
+    <div class="media-body" v-on:click="toggleMeaning">
+      <p>
+        <span>{{ word.word }}</span>
+        <span v-if="word.ruby">&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;{{ word.ruby }}</span>
+      </p>
+      <p>
+        <small v-if="shouldShowMeaning">{{ word.meaning }}</small>
+        <br>
+      </p>
     </div>
-    <button v-bind:class="'btn ' + pressStateClass" v-on:click="toggleCheck"><span class="icon icon-close"></span></button>
+    <button v-bind:class="'btn ' + pressStateClass" v-on:click="toggleCheck">
+      <span class="icon icon-close"></span>
+    </button>
   </li>
 </template>
 
 <script>
-import repos from '../lib/localStrageRepos'
-// class="btn btn-negative"
+import repos from '@/lib/localStrageRepos.js';
+
+const BTN_PRESSED_CLASS_NAME = 'btn-negative';
 
 export default {
-  name: 'Word',
-  props: ['word'],
-  data() {
-      return {
-          pressStateClass: ''
-      }
-  },
-  created() {
-    //   this.pressStateClass = 'btn-negative'
-      this.pressStateClass = this.isSaved() ? 'btn-negative' : ''
-  },
-  methods: {
-      toggleCheck(e) {
-          if (this.pressStateClass === '') {
-              this.pressStateClass = 'btn-negative'
-                repos.addUncheckedWord(this.word.langId, this.word.id)
-          } else {
-              this.pressStateClass = ''
-              repos.deleteUncheckedWord(this.word.langId, this.word.id)
-          }
-      },
-      isSaved() {
-          return repos.getUncheckedWords(this.word.langId)[this.word.id] ? true : false
-      }
-  }
-
+    name: 'Word',
+    props: ['word'],
+    data() {
+        return {
+            pressStateClass: '',
+            shouldShowMeaning: false,
+        };
+    },
+    created() {
+        this.pressStateClass = this.isSaved() ? BTN_PRESSED_CLASS_NAME : '';
+    },
+    methods: {
+        toggleCheck() {
+            if (this.pressStateClass === '') {
+                this.pressStateClass = BTN_PRESSED_CLASS_NAME;
+                repos.addUncheckedWord(this.word.langId, this.word.id);
+            } else {
+                this.pressStateClass = '';
+                repos.deleteUncheckedWord(this.word.langId, this.word.id);
+            }
+        },
+        isSaved() {
+            return !!repos.getUncheckedWords(this.word.langId)[this.word.id];
+        },
+        toggleMeaning() {
+            this.shouldShowMeaning = !this.shouldShowMeaning;
+        },
+    },
 };
 </script>
---
+++ src/lib/localStrageRepos.js
@@ -0,0 +1,34 @@
+
+function makeKey(langId) {
+    return `langId-${langId}`;
+}
+
+export default {
+
+    getItem(key) {
+        localStorage.getItem(key);
+    },
+
+    setItem(key, value) {
+        localStorage.setItem(key, value);
+    },
+
+
+    getUncheckedWords(langId) {
+        const value = localStorage.getItem(makeKey(langId)) || '{}';
+        return JSON.parse(value);
+    },
+
+    addUncheckedWord(langId, wordId) {
+        const wordIdObjects = this.getUncheckedWords(langId);
+        wordIdObjects[wordId] = true;
+        localStorage.setItem(makeKey(langId), JSON.stringify(wordIdObjects));
+    },
+
+    deleteUncheckedWord(langId, wordId) {
+        const wordIdObjects = this.getUncheckedWords(langId);
+        delete wordIdObjects[wordId];
+        localStorage.setItem(makeKey(langId), JSON.stringify(wordIdObjects));
+    },
+
+};
